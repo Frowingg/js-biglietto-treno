3e736b819c1a7ea0af0b2e23754839f4ad1d2ca3
Frowingg <edbico0@gmail.com>
2022-06-01 17:10:19 +0200

    da rifinire

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 //Creo le variabile (costoPerKm / age) / clientKm)
-const ticketCostPerKm = 0.21;
 const clientAge = parseInt(prompt('Quanti anni hai?'));
 const clientKm = parseInt(prompt('Quanti km vuoi percorrere?'));
-let userMessage = 'nessuno sconto applicato'
-console.log(ticketCostPerKm)
+let userMessage = ' '
 console.log(clientAge)
 console.log(clientKm)
 console.log(userMessage)
@@ -12,19 +10,17 @@ console.log(userMessage)
 let clientTicketCost = clientKm*0.21
 console.log(clientTicketCost)
 
+
 //in base alle risposte o applico lo sconto del 20% o del 40% o nessuno sconto
  if (clientAge < 18) {
-     clientTicketCost = clientTicketCost%100*20
-     userMessage = 'abbiamo applicato lo sconto del 20% poichè sei minorenne'
+     clientTicketCost = ((clientKm*0.21)/100*20)
+     userMessage = ', abbiamo applicato lo sconto del 20% poichè sei minorenne'
+} else if (clientAge > 64) {
+    clientTicketCost = ((clientKm*0.21)/100*40)
+    userMessage = ', abbiamo applicato lo sconto del 40% poichè lei ha più di 64 anni'
 }
 console.log(clientTicketCost)
 console.log(userMessage)
 
-alert(`il costo del biglietto è di ${clientTicketCost}, $, ${userMessage}`)
-// else (clientAge <  64) {
-//     clientTicketCost = clientTicketCost%100*40
-//     userMessage = 'abbiamo applicato lo sconto del 40% poichè lei ha più di 65 anni'
-// }
-
-// alert(userMassege + clientTicketCost)
+alert(`il costo del biglietto è di ${clientTicketCost} $ ${userMessage}`)
 
